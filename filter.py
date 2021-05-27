e80b4af955ca4e820b266ae548f2fab5bbd47884
vporyadke <mczalyalov@gmail.com>
2021-05-27 11:35:17 +0300

    merge bed files

--- filter.py
+++ filter.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-files = ["ENCFF540NGG.bed",  "ENCFF540NGG.hg19.bed", "ENCFF759NWD.bed",  "ENCFF759NWD.hg19.bed"]
+files = ["H3K4me1_K562.merge.hg19.bed"]
 for filename in files:
-    df = pd.read_table(filename, names=["chrom", "start", "end", "name", "score", "a", "b", "c", "d", "e"])
+    df = pd.read_table("data/" + filename, names=["chrom", "start", "end", "name", "score", "a", "b", "c", "d", "e"])
     lens = df["end"] - df["start"]
     df_filtered = df[lens < 5000]
     plt.hist(lens[lens < 5000])
-    plt.savefig(filename[:-3] + "hist.png")
+    plt.savefig("images/" + filename[:-3] + "hist.png")
     plt.close()
-    df_filtered[["chrom", "start", "end", "name", "score"]].to_csv(filename[:-3] + "filtered.bed", sep="\t")
+    df_filtered[["chrom", "start", "end", "name", "score"]].to_csv("data/" + filename[:-3] + "filtered.bed", sep="\t")
 
